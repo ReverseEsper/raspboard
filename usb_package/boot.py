ce76734037191f0543d907658cb90ff761b8c6d5
Adam Kurowski <adam.kurowski@stepstone.com>
2022-05-29 12:06:39 +0200

    Update to add auto open function

--- usb_package/boot.py
+++ usb_package/boot.py
@@ -1,6 +1,6 @@
 import storage
 import board, digitalio
-
+import microcontroller
 
 button = digitalio.DigitalInOut(board.GP28)
 button.direction = digitalio.Direction.INPUT
@@ -8,4 +8,7 @@ button.pull = digitalio.Pull.DOWN
 
 # To access USB disk, press Left Upper button during bootup.
 if not button.value:
+	microcontroller.nvm[0]=1
 	storage.disable_usb_drive()
+else:
+	microcontroller.nvm[0]=2
--- usb_package/code.py
+++ usb_package/code.py
@@ -6,26 +6,43 @@ import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
+import microcontroller
 
 # Define some Variables to use later
 keyboard = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(keyboard)
 buttons = {}
-button_map = (
-    #Name of Button, Pin on Board, Action to take, Value of that action
-    ('UpLeft',board.GP28,'keypress',Keycode.HOME),
-    ('UpRight',board.GP6,'keypress',Keycode.END),
-    ('Left',board.GP14,'keypress',Keycode.LEFT_ARROW),
-    ('Right',board.GP8,'keypress',Keycode.RIGHT_ARROW),
-    ('Up',board.GP12,'keypress',Keycode.UP_ARROW),
-    ('Down',board.GP10,'keypress',Keycode.DOWN_ARROW)    
-)
+startup = microcontroller.nvm[0]
+if startup == 2:
+        button_map = (
+        # If You started Board with Upper Left button pressed, aside from enabling Drive, that button will now perform different funcion
+        ('UpLeft',board.GP28,'function','show_github_on_windows'),
+        ('UpRight',board.GP6,'keypress',Keycode.PAGE_DOWN),
+        ('Left',board.GP14,'keypress',Keycode.LEFT_ARROW),
+        ('Right',board.GP8,'keypress',Keycode.RIGHT_ARROW),
+        ('Up',board.GP12,'keypress',Keycode.UP_ARROW),
+        ('Down',board.GP10,'keypress',Keycode.DOWN_ARROW)
+    )
+else:
+    button_map = (
+        # Name of Button, Pin on Board, Action to take, Value of that action
+        # Button codes are here : https://docs.circuitpython.org/projects/hid/en/latest/api.html#adafruit-hid-keycode-keycode
+        ('UpLeft',board.GP28,'keypress',Keycode.PAGE_UP),
+        ('UpRight',board.GP6,'keypress',Keycode.PAGE_DOWN),
+        ('Left',board.GP14,'keypress',Keycode.LEFT_ARROW),
+        ('Right',board.GP8,'keypress',Keycode.RIGHT_ARROW),
+        ('Up',board.GP12,'keypress',Keycode.UP_ARROW),
+        ('Down',board.GP10,'keypress',Keycode.DOWN_ARROW)
+    )
+
+
+
 
 # Sleep so HID has time to set up keyboard
 time.sleep(3)
 
-# Board RP2040 Zero constains RGB led called NeoPixel. 
-# It actually is microcontroller on itself and you can control it 
+# Board RP2040 Zero constains RGB led called NeoPixel.
+# It actually is microcontroller on itself and you can control it
 pixels = neopixel.NeoPixel(board.GP16, 1)
 pixels.brightness = 0.01
 
@@ -33,19 +50,15 @@ pixels.brightness = 0.01
 # First it show how to use function layout.write , type some text as keyboard
 # Then it show how to press specific keys on keyboard
 # Map Example : ('UpLeft',board.GP28,'function','button_action_1'),
-def button_action_1():
-    print("I'll perform special Action !")
+def show_github_on_windows():
     pixels.brightness = 0.5
+    # Green Color
     pixels.fill((255, 0, 0))
-    # Example 1 : Writing Some text :
-    layout.write("Never Gona Give you up!\n")
+    keyboard.send(Keycode.CONTROL,Keycode.ESCAPE)
+    time.sleep(1)
+    layout.write("https://github.com/ReverseEsper/raspboard\n")
     time.sleep(0.5)
-    layout.write("RickRoll!!!!!")
-    time.sleep(0.5)
-    # Example 2 : Pressing Keys 
-    keyboard.press(Keycode.SHIFT, Keycode.X)
-    keyboard.release_all
-
+    # Example 2 : Pressing Keys
     pixels.brightness = 0.01
 
 
@@ -57,10 +70,9 @@ for button in button_map:
     buttons[b_name].direction = digitalio.Direction.INPUT
     buttons[b_name].pull = digitalio.Pull.DOWN
 
-
 # Loop for checking for buttons
 while True:
-    pixels.fill((0, 255, 0))
+    pixels.fill((0,255,0))
     for button in button_map:
         b_name = button[0]
         b_action = button[2]
